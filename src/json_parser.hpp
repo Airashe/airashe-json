ff707ab2e29f7dd6131393ad42c729fe9dd59e80
Airashe <eblludu247@gmail.com>
2024-05-02 20:21:37 +0300

    Install public headers with right structure.

--- src/json_parser.hpp
+++ src/json_parser.hpp
@@ -1,16 +1,10 @@
 #pragma once
 
-#ifdef _WIN32
-  #define HELLO_EXPORT __declspec(dllexport)
-#else
-  #define HELLO_EXPORT
-#endif
-
 namespace airashe::json
 {
   class json_parser
   {
-  public: 
-    void parse(const char* json);
+  public:
+    void parse(const char *json);
   };
 }
---
+++ src/structs/jtoken.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace airashe::json
+{
+    class jtoken final
+    {
+    public:
+        const char *name;
+    };
+}
